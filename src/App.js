2c81dcba9441a73f7fffb0a4385c31857e6d9d0c
jQN <jqn@jQNs-MacBook-Pro.local>
2019-10-03 21:11:04 +0200

    chore: added two base components and viewport metatag

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import React, { Component } from "react";
-
-import { hot } from "react-hot-loader";
+import { ThemeProvider } from "@material-ui/styles";
+import { createMuiTheme } from "@material-ui/core/styles";
+import { blue, indigo } from "@material-ui/core/colors";
 import "typeface-roboto";
 import "./styles.css";
+import { hot } from "react-hot-loader";
+
+const theme = createMuiTheme({
+  palette: {
+    secondary: {
+      main: blue[900],
+    },
+    primary: {
+      main: indigo[700],
+    },
+  },
+  typography: {
+    // Use the system font instead of the default Roboto font.
+    fontFamily: ['"Lato"', "sans-serif"].join(","),
+  },
+});
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1> Hello, World! </h1>
+        <ThemeProvider theme={theme}>
+          <h1> Hello, World! </h1>
+        </ThemeProvider>
       </div>
     );
   }
